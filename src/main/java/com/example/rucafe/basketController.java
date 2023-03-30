b5f13fa6407da58756567cf4aaf40e9a8b761152
Medhasri Veldurthi <medhasri.vel@gmail.com>
2023-03-30 11:26:48 -0400

    creating basket view + controller

---
+++ src/main/java/com/example/rucafe/basketController.java
@@ -0,0 +1,14 @@
+package com.example.rucafe;
+
+public class basketController {
+    private MainController mainController;
+
+    public basketController() {
+    }
+
+    public void setMainController(MainController controller) {
+        this.mainController = controller;
+    }
+}
+
+
