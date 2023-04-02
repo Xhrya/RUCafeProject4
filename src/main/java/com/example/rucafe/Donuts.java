f88100d94074b391c51481ca00712968bdd2b7f5
Shreya Pandey <42945783+Xhrya@users.noreply.github.com>
2023-04-02 12:02:08 -0400

    created a link for store orders and basket orders

--- src/main/java/com/example/rucafe/Donuts.java
+++ src/main/java/com/example/rucafe/Donuts.java
@@ -5,7 +5,24 @@ import java.util.ArrayList;
 
 public class Donuts extends MenuItem {
 
-  
+
+  private final double yeast = 1.59;
+  private final double cake = 1.79;
+
+  private final double hole = 1.39;
+//
+//        if(donutType.equals("yeast"))
+//    {
+//        total = 1.59 * numberOfDonuts;
+//    }
+//        else if(donutType.equals("cake")) {
+//        total = 1.79 * numberOfDonuts;
+//
+//    }
+//        else if(donutType.equals("hole"))
+//    {
+//        total = 0.39 * numberOfDonuts;
+//    }
     @Override
     public double itemPrice() {
        return 0.0;
--- src/main/java/com/example/rucafe/StoreOrderController.java
+++ src/main/java/com/example/rucafe/StoreOrderController.java
@@ -7,17 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class StoreOrderController extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StoreOrderController.class.getResource("storeOrderView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 550, 700);
-        stage.setTitle("Welcome to RuCafe!");
-        stage.setScene(scene);
-        stage.show();
-    }
+public class StoreOrderController {
+    private MainController mainController;
 
-    public static void main(String[] args) {
-        launch();
+    public void setMainController(MainController controller) {
+        mainController = controller;
     }
 }
