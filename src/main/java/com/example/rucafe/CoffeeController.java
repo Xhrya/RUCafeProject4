c76bfb8a8b8bedbf3905c53fdc86f4b18a57f4fe
Medhasri Veldurthi <medhasri.vel@gmail.com>
2023-04-01 20:20:01 -0400

    coffee controller update

--- src/main/java/com/example/rucafe/CoffeeController.java
+++ src/main/java/com/example/rucafe/CoffeeController.java
@@ -1,5 +1,6 @@
 package com.example.rucafe;
 
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
@@ -9,9 +10,8 @@ import javafx.scene.control.ToggleGroup;
 import java.util.ArrayList;
 
 public class CoffeeController {
-
     private MainController mainController;
-
+    private int quantity = 0;
 
     public void setMainController(MainController controller) {
         mainController = controller;
@@ -19,58 +19,45 @@ public class CoffeeController {
 
     @FXML
     ToggleGroup cupSize;
-
     @FXML
     CheckBox SweetCream;
-
     @FXML
     CheckBox FrenchVanilla;
-
     @FXML
     CheckBox IrishCream;
-
     @FXML
     CheckBox Caramel;
-
     @FXML
     CheckBox Mocha;
-
     @FXML
     MenuButton CoffeeQty;
 
+    public void oneCoffee(ActionEvent actionEvent) {quantity = 1;}
+    public void twoCoffee(ActionEvent actionEvent) {quantity = 2;}
+    public void threeCoffee(ActionEvent actionEvent) {quantity = 3;}
+    public void fourCoffee(ActionEvent actionEvent) {quantity = 4;}
+    public void fiveCoffee(ActionEvent actionEvent) {quantity = 5;}
 
-    protected void onAddToBasketClick(Event e){
+    @FXML
+    protected void onAddToBasket(Event e){
         if(cupSize.getSelectedToggle() == null){
             //insert text here
             return;
         }
+        String size = cupSize.getSelectedToggle().toString();
+        size = size.substring(size.indexOf("'") +1, size.length() -1);
+
         ArrayList addIns = new ArrayList();
+        if(SweetCream.isSelected()){addIns.add("Sweet Cream");}
+        if(FrenchVanilla.isSelected()){addIns.add("French Vanilla");}
+        if(IrishCream.isSelected()){addIns.add("Irish Cream");}
+        if(Caramel.isSelected()){addIns.add("Caramel");}
+        if(Mocha.isSelected()){addIns.add("Mocha");}
 
-        int qty = Integer.parseInt(CoffeeQty.getPopupSide().toString());
-
-        if(SweetCream.isSelected()){
-            addIns.add("Sweet Cream");
-        }
-
-        if(FrenchVanilla.isSelected()){
-            addIns.add("French Vanilla");
-        }
-
-        if(IrishCream.isSelected()){
-            addIns.add("Irish Cream");
-        }
-
-        if(Caramel.isSelected()){
-            addIns.add("Caramel");
-        }
-
-        if(Mocha.isSelected()){
-            addIns.add("Mocha");
-        }
-
-        for(int i=0; i<qty; i++){
-            Coffee newCoffee = new Coffee(cupSize.getSelectedToggle().toString(), addIns);
+        for(int i=0; i<quantity; i++){
+            Coffee newCoffee = new Coffee(size, addIns);
             //somehow adds to the basket
         }
+        quantity = 0;
     }
 }
