6fc72cefe0cdc855b8256fc5287e77629bdd5701
Medhasri Veldurthi <medhasri.vel@gmail.com>
2023-04-01 21:15:59 -0400

    coffee controller pricing

--- src/main/java/com/example/rucafe/CoffeeController.java
+++ src/main/java/com/example/rucafe/CoffeeController.java
@@ -3,15 +3,14 @@ package com.example.rucafe;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 
 import java.util.ArrayList;
 
 public class CoffeeController {
     private MainController mainController;
     private int quantity = 0;
+    private double currentPrice = 0.0;
 
     public void setMainController(MainController controller) {
         mainController = controller;
@@ -31,12 +30,25 @@ public class CoffeeController {
     CheckBox Mocha;
     @FXML
     MenuButton CoffeeQty;
+    @FXML
+    TextField priceTotal;
 
-    public void oneCoffee(ActionEvent actionEvent) {quantity = 1;}
-    public void twoCoffee(ActionEvent actionEvent) {quantity = 2;}
-    public void threeCoffee(ActionEvent actionEvent) {quantity = 3;}
-    public void fourCoffee(ActionEvent actionEvent) {quantity = 4;}
-    public void fiveCoffee(ActionEvent actionEvent) {quantity = 5;}
+
+    public void oneCoffee(ActionEvent actionEvent) {quantity = 1;
+//        priceTotal.appendText("" + currentPrice);
+    }
+    public void twoCoffee(ActionEvent actionEvent) {quantity = 2;
+//        priceTotal.appendText("" + currentPrice * 2);
+    }
+    public void threeCoffee(ActionEvent actionEvent) {quantity = 3;
+//        priceTotal.appendText("" + currentPrice * 3);
+    }
+    public void fourCoffee(ActionEvent actionEvent) {quantity = 4;
+//        priceTotal.appendText("" + currentPrice * 4);
+    }
+    public void fiveCoffee(ActionEvent actionEvent) {quantity = 5;
+//        priceTotal.appendText("" + currentPrice * 5);
+    }
 
     @FXML
     protected void onAddToBasket(Event e){
@@ -47,17 +59,46 @@ public class CoffeeController {
         String size = cupSize.getSelectedToggle().toString();
         size = size.substring(size.indexOf("'") +1, size.length() -1);
 
+
         ArrayList addIns = new ArrayList();
-        if(SweetCream.isSelected()){addIns.add("Sweet Cream");}
-        if(FrenchVanilla.isSelected()){addIns.add("French Vanilla");}
-        if(IrishCream.isSelected()){addIns.add("Irish Cream");}
-        if(Caramel.isSelected()){addIns.add("Caramel");}
-        if(Mocha.isSelected()){addIns.add("Mocha");}
+        if(SweetCream.isSelected()){
+            addIns.add("Sweet Cream");
+            currentPrice = currentPrice + 0.30;
+        }
+        if(FrenchVanilla.isSelected()){
+            addIns.add("French Vanilla");
+            currentPrice = currentPrice + 0.30;
+        }
+        if(IrishCream.isSelected()){
+            addIns.add("Irish Cream");
+            currentPrice = currentPrice + 0.30;
+        }
+        if(Caramel.isSelected()){
+            addIns.add("Caramel");
+            currentPrice = currentPrice + 0.30;
+        }
+        if(Mocha.isSelected()){
+            addIns.add("Mocha");
+            currentPrice = currentPrice + 0.30;
+        }
+
+        if(size.equals("Short")){
+            currentPrice = currentPrice + 1.89;
+        }else if(size.equals("Tall")){
+            currentPrice = currentPrice + 2.29;
+        }else if(size.equals("Grande")){
+            currentPrice = currentPrice + 2.69;
+        }else if(size.equals("Venti")){
+            currentPrice = currentPrice + 3.09;
+        }
 
         for(int i=0; i<quantity; i++){
             Coffee newCoffee = new Coffee(size, addIns);
             //somehow adds to the basket
+
         }
+        priceTotal.appendText("$" + (currentPrice * quantity));
         quantity = 0;
     }
+
 }
